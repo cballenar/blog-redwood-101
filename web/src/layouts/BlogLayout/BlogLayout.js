b49ee864daf2cf4158e590f3d608380e110cb9b9
Carlos Ballena <cballenar@gmail.com>
2022-06-15 11:03:49 -0400

    Added layout

---
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -0,0 +1,22 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const BlogLayout = ({ children }) => {
+  return (
+    <>
+      <header>
+        <h1>RW Blog</h1>
+        <nav><ul>
+          <li><Link to={routes.home()}>Home</Link></li>
+          <li><Link to={routes.about()}>About</Link></li>
+        </ul></nav>
+      </header>
+
+      <main>
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default BlogLayout
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -5,23 +5,13 @@ const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
-      <header>
-        <h1>RW Blog</h1>
-        <nav><ul>
-          <li><Link to={routes.home()}>Home</Link></li>
-          <li><Link to={routes.about()}>About</Link></li>
-        </ul></nav>
-      </header>
-
-      <main>
-        <p>
-          Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-        </p>
-        <p>
-          My default route is named <code>home</code>, link to me with `
-          <Link to={routes.home()}>Home</Link>`
-        </p>
-      </main>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
     </>
   )
 }
